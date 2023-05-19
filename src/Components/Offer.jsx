b90663127ecb6d9f88eaf81698de497df7a6a968
SamyKhelfa <60275651+SamyKhelfa@users.noreply.github.com>
2023-05-19 14:54:17 +0200

    up

--- src/Components/Offer.jsx
+++ src/Components/Offer.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
 const Offer = () => {
   const { id } = useParams();
+  const [data, setData] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios.get(
-        "https://lereacteur-vinted-api.herokuapp.com/offers/{$id}"
+        `https://lereacteur-vinted-api.herokuapp.com/offers/${id}`
       );
-      setData(result.data.offers._id);
+      setData(result.data);
     };
     fetchData();
-  }, []);
+  }, [id]);
 
   return (
     <div>
-      <h2>{item.product_name}</h2>
-      <img src={item.product_image.url} alt={item.product_name} />
-      <p>{item.product_price} €</p>
-      <p>{item.product_details[1].TAILLE}</p>
-      <p>{item.product_details[0].MARQUE}</p>
+      {data ? (
+        <>
+          <h2>{data.product_name}</h2>
+          <img src={data.product_image.url} alt={data.product_name} />
+          <p>{data.product_price} €</p>
+          <p>{data.product_details[1].TAILLE}</p>
+          <p>{data.product_details[0].MARQUE}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
